c15f8eadc9782b090dc21e8ef1b7fddc060ca7e9
Han Xu <hanxu27@gmail.com>
2019-05-21 17:22:35 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,26 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params)
-	  @post.save
+		# byebug
+		@post = Post.create(title: params[:post][:title].titleize, description: params[:post][:description])
+	  # @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post))
+		@post = Post.find(params[:id])
+		@post.update(params.require(:post).permit(:title, :description))
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	private
+
+	def strong_params
+		params.require(:post).permit(:title, :description)
+	end
+
 end
